59d605e71171ebe1b7b05bda2c3c429b48086264
Live session user <sandeep.k@betsol.com>
2016-08-21 05:32:15 +0000

    Controllers created for SignUp

--- app/models/Profile.java
+++ app/models/Profile.java
@@ -5,25 +5,24 @@ import com.avaje.ebean.Model;
 import javax.annotation.Generated;
 import javax.persistence.*;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by lubuntu on 8/20/16.
  */
 @Entity
 public class Profile extends Model {
+
+    public Profile(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     public Long id;
-    public String email;
-    public String password;
+    public String firstName;
+    public String lastName;
+    public String company;
 
-    @OneToMany(mappedBy = "sender")
-    public List<ConnectionRequest> connectionRequestsSent;
-
-    @OneToMany(mappedBy = "receiver")
-    public List<ConnectionRequest> connectionRequestsReceived;
-
-    @OneToOne
-    public Profile profile;
 
 }
